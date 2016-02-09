8d41983122740a39e1909a5f53a6c30855d183ca
mrothenbuhler <mrothenbuhler@trulia.com>
2016-02-08 20:39:23 -0800

    alskdjf

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-var project = require('./routes/project');
+var home = require('./routes/home');
 // Example route
 // var user = require('./routes/user');
 
@@ -37,7 +37,8 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
-app.get('/project/:id', project.projectInfo);
+app.get('/home', home.view);
+
 // Example route
 // app.get('/users', user.list);
 
