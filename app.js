fc8b5cd38c654f099b99fd725bcff11c558415d6
darmadoo <markdarmadi@gmail.com>
2016-02-09 14:23:38 -0800

    DB worked

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var home = require('./routes/home');
+var data = require('./routes/data');
 // Example route
 // var user = require('./routes/user');
 
@@ -38,6 +39,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/home', home.view);
+app.get('/data', data.randomPalette);
 
 // Example route
 // app.get('/users', user.list);
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,9 +9,15 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+	// $('.project a').click(addProjectDetails);
 
-	$('#colorBtn').click(randomizeColors);
+	// $('#colorBtn').click(randomizeColors);
+
+	$(".login-btn").click(check);
+}
+
+function check(e){
+	e.preventDefault();
 }
 
 /*
@@ -26,7 +32,23 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, projName);
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function projName(result){
+	console.log(result);
+	console.log($("#project" + result.id + " .thumbnail .details"));
+	var curProj = $("#project" + result.id + " .thumbnail .details");
+
+	var innerHTML = '<a href="#" class="thumbnail"> ' + 
+					'<img src="' + result['image'] + '" class="detailsImage">' + 
+					'<p>' + result['title'] + '<p>' + 
+					'<p><small>' + result['date'] + '</small></p>' + 
+					'<p><small>' + result['summary'] + '</small></p></a>';
+
+	curProj.html(innerHTML);
 }
 
 /*
@@ -35,4 +57,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/data", color);
+}
+
+function color(result){
+	console.log(result);
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+					
 }
