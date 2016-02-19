1071b8d941a690967fb346df4ce31b73568d5b95
darmadoo <markdarmadi@gmail.com>
2016-02-19 13:35:53 -0800

    meow

--- routes/home.js
+++ routes/home.js
@@ -1,33 +1,33 @@
 var data = require("../data.json");
 
-exports.view = function(req, res){
-  res.render('home');
+exports.view = function(req, res) {
+    res.render('home');
 };
 
-exports.receive = function(req, res){
-	//console.log(req);
-	if(req != null){
-		var user = req.body.user;
-		var pass = req.body.password;
-		var flag = false;
-		var first = "";
-		var last = "";
+exports.receive = function(req, res) {
+    //console.log(req);
+    if (req != null) {
+        var user = req.body.user;
+        var pass = req.body.password;
+        var flag = false;
+        var first = "";
+        var last = "";
 
-		for(var i = 0; i < data["users"].length; i++){
-			if(data["users"][i].email == user && data["users"][i].password == pass){
-				first = data["users"][i].firstName;
-				last = data["users"][i].lastName;
-				flag = true;
-			}
-		}
-		if (flag) {
-			data["current"].push(first+ " " + last);
-			res.render('home', {
-				'first' : first,
-				'last' : last
-			});
-		} else {
-			res.render('index');
-		}
-	}
+        for (var i = 0; i < data["users"].length; i++) {
+            if (data["users"][i].email == user && data["users"][i].password == pass) {
+                first = data["users"][i].firstName;
+                last = data["users"][i].lastName;
+                flag = true;
+            }
+        }
+        if (flag) {
+            data["current"].push(first + " " + last);
+            res.render('home', {
+                'first': first,
+                'last': last
+            });
+        } else {
+            res.render('index');
+        }
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -3,51 +3,51 @@
  */
 var data = require("../data.json");
 
-exports.view = function(req, res){
-	var first = req.body.firstname;
-	var last = req.body.lastname;
-	var email = req.body.email;
-	var password = req.body.pass;
-	var cpass = req.body.cpass;
+exports.view = function(req, res) {
+    var first = req.body.firstname;
+    var last = req.body.lastname;
+    var email = req.body.email;
+    var password = req.body.pass;
+    var cpass = req.body.cpass;
 
-	if(password != cpass){
-		console.log("GG");
-	}
+    if (password != cpass) {
+        console.log("GG");
+    }
 
-	var newEntry = {
-						"email" : email,
-				        "userName": "mrothenbuhler",
-				        "firstName": first,
-				        "lastName": last,
-				        "DOB": "01/03/1994",
-				        "hobbies": [
-				          "hearthstone",
-				          "running",
-				          "soccer"
-				        ],
-				        "password" : password
-				   }
+    var newEntry = {
+        "email": email,
+        "userName": "mrothenbuhler",
+        "firstName": first,
+        "lastName": last,
+        "DOB": "01/03/1994",
+        "hobbies": [
+            "hearthstone",
+            "running",
+            "soccer"
+        ],
+        "password": password
+    }
 
-	data["users"].push(newEntry);
-	res.render('index');
+    data["users"].push(newEntry);
+    res.render('index');
 };
 
-exports.search = function(req, res){
-  res.render('search');
+exports.search = function(req, res) {
+    res.render('search');
 };
 
-exports.my_profile = function(req,res){
-  res.render('my_profile');
+exports.my_profile = function(req, res) {
+    res.render('my_profile');
 }
 
 exports.my_lunches = function(req, res) {
-	res.render('my_lunches');
+    res.render('my_lunches');
 }
 
 exports.confirmation = function(req, res) {
-	res.render('confirmation');
+    res.render('confirmation');
 }
 
 exports.current_room = function(req, res) {
-	res.render('current_room');
+    res.render('current_room');
 }
